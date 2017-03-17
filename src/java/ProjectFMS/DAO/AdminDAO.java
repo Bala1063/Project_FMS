d8d650d16aeb662108152145510f0f46ad291fba
Bala1063 <balaji.scse@ksrce.ac.in>
2017-03-17 14:06:39 +0530

    modified

---
+++ src/java/ProjectFMS/DAO/AdminDAO.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ProjectFMS.DAO;
+
+import ProjectFMS.Bean.DailyTaskAllocationBean;
+import ProjectFMS.Bean.QuestionBean;
+import ProjectFMS.Bean.TrainerBean;
+import ProjectFMS.Bean.TrainingBean;
+import ProjectFMS.Util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author bala
+ */
+public class AdminDAO {
+    public String addOrUpdateTrainerDetails(TrainerBean trainerBean) {
+        
+        Session session = Util.getSessionFactory().openSession();
+        Transaction t = null;
+        try {
+            t = session.beginTransaction();
+           session.saveOrUpdate(trainerBean);
+            t.commit();
+        } catch (HibernateException e) {
+            if (t != null) {
+                t.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+       return "success";
+    }
+    
+    public String addOrUpdateTrainingDetails(TrainingBean trainingBean) {
+        
+        Session session = Util.getSessionFactory().openSession();
+        Transaction t = null;
+        try {
+            t = session.beginTransaction();
+           session.saveOrUpdate(trainingBean);
+            t.commit();
+        } catch (HibernateException e) {
+            if (t != null) {
+                t.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+       return "success";
+    }
+    
+     public String addOrUpdateQuestion(QuestionBean questionBean){
+         
+        Session session = Util.getSessionFactory().openSession();
+         Transaction t = null;
+        try {
+            t = session.beginTransaction();
+            session.saveOrUpdate(questionBean);
+            t.commit();
+        } catch (HibernateException e) {
+            if (t != null) {
+                t.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return "success";
+    }
+     
+    
+     public String addOrUpdateDailyTaskAllocation(DailyTaskAllocationBean dailyTaskAllocationBean){
+        Session session = Util.getSessionFactory().openSession();
+         Transaction t = null;
+        try {
+            t = session.beginTransaction();
+            session.saveOrUpdate(dailyTaskAllocationBean);
+            t.commit();
+        } catch (HibernateException e) {
+            if (t != null) {
+                t.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return "success";
+    }
+}
