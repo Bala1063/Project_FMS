5e24736ebfffcf96ece24e42939b533f50830f3e
ArunaMani <aruna.mani28@gmail.com>
2017-03-24 09:47:18 +0530

    ar

--- src/java/ProjectFMS/DAO/AdminDAO.java
+++ src/java/ProjectFMS/DAO/AdminDAO.java
@@ -5,10 +5,25 @@
  */
 package ProjectFMS.DAO;
 
+import ProjectFMS.Util.Util;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 /**
  *
  * @author bala
  */
 public class AdminDAO {
+    public String deleteContentsByContentId(List<String> contentIds){
+ 
+        Session session=Util.getSessionFactory().openSession();
+        for(int i=0;i<contentIds.size();i++){
+            Query query = session.createSQLQuery("delete from Content_tb where Content_id='"+contentIds.get(i)+"'");
+        }
+        session.close();
+        return "success";
+    }
     
+    public String deleteDailyTaskAllocation
 }
